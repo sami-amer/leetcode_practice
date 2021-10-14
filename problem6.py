7b3d5fbdb8abb912eea330a846e0426a09ea09fc
sami-amer <sami.amer@neo.learn.ventures>
2021-10-13 21:52:09 -0400

    mend

--- problem6.py
+++ problem6.py
@@ -1,17 +1,19 @@
 def convert(s, numRows):
-    rows, row = min(numRows, len(s)), 0
-    arr, flag = ["" for _ in range(rows)], 1
+    rows = min(numRows, len(s))
+    row = 0
+    arr = ["" for _ in range(rows)]
+    flag = -1 #initialize to down
 
     for letter in s:
         if row == rows:
-            flag = -1
-            row = max(row-2,0)
+            flag = 1 # we want to go up
+            row = max(row-2, 0) # go back to first row or connector row
         if row == 0:
-            flag = 1
-        if flag == -1:
+            flag = -1
+        if flag == 1:
             arr[row] += letter
             row -= 1
-        if flag == 1:
+        if flag == -1:
             arr[row] += letter
             row += 1
     return "".join(arr)
