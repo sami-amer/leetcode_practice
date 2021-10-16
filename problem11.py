f1c446e659c59e80911ce0f05210e92ee88d6e2d
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2021-10-15 20:50:15 -0400

    solution to LC11

--- problem11.py
+++ problem11.py
@@ -1,38 +1,20 @@
-def calcArea(height, i,j):
-    # print(i,j)
-    return min(height[i], height[j]) * abs(j - i)
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        left = 0 # We start by holding the left 
+        right = len(height) - 1 # and right poles
+        mx = 0
+        curr_area = 0
 
-def maxArea(height):
-    # O (N^2) solution
-    # curr_max = 0
-    # for i in range(len(height)):
-    #     for j in range(1,len(height)):
-    #         area = min(height[i], height[j]) * (j - i)
-    #         curr_max = area if area > curr_max else curr_max
-    # return curr_max
-    ## The above times out in LC
-    left = 0 # We start by holding the left 
-    right = len(height) - 1 # and right poles
-    maxes = set() # All our possible answers are here: we only need to return the maximum size possible
-    maxes.add(calcArea(height,left,right))
-    while left < right -1:
-        if calcArea(height, left+1, right) > max(maxes) and calcArea(height, left, right - 1) > max(maxes):
-            maxes.add(calcArea(height, left, right - 1))
-            maxes.add(calcArea(height, left + 1, right))
-            right -=1
-            left += 1
-        elif calcArea(height, left+1, right) > max(maxes):
-            maxes.add(calcArea(height, left+1, right))
-            left += 1
-        elif calcArea(height, left, right - 1) > max(maxes):
-            maxes.add(calcArea(height, left, right - 1))
-            right -= 1
-        else:
-            maxes.add(maxArea(height[left+1:right]))
-            maxes.add(maxArea(height[left:right - 1]))
-            left+=1
-            right-=1
-    return max(maxes)
+        while left < right:
+            if height[left] < height[right]:
+                curr_area = min(height[left], height[right]) * (right - left)
+                left += 1
+            else:
+                curr_area = min(height[left], height[right]) * (right - left)
+                right -= 1
+            mx = max(curr_area, mx)
+        return mx
+
 
 
 
@@ -43,4 +25,4 @@ if __name__ == "__main__":
     input2 = [1,3,2,5,25,24,5]
     input3 = [1,2,3,4,5,25,24,3,4]
     
-    print(maxArea(input3))
+    print(Solution.maxArea(None, input3))
