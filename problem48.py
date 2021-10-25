8383c60116c2c7000d3d2820d857e4e3cc0c6a2f
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2021-10-25 01:02:47 -0400

    a more proper but super slow solution to LC48

--- problem48.py
+++ problem48.py
@@ -5,7 +5,14 @@ class Solution:
         """
         # We can't create a new matrix, but we do at minimum need to hold a number or a row.
         
-        matrix[:] = zip(*matrix[::-1])
+        # matrix[:] = zip(*matrix[::-1])
+        n = len(matrix)
+
+        matrix.reverse()
+        
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
 
 
 
@@ -14,4 +21,5 @@ class Solution:
 if __name__ == "__main__":
     input1 = [[1,2,3],[4,5,6],[7,8,9]]
     # OUTPUT: [[7,4,1],[8,5,2],[9,6,3]]
+    Solution.rotate(None,input1)
     print(input1)
