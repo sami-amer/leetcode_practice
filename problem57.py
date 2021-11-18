ccaf4bdb0f961b30063b554f2afb9cc00ae4fd46
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2021-11-18 00:37:22 -0500

    LC57 solution; better version of LC56 but with an insert

---
+++ problem57.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        intervals.append(newInterval)
+        intervals.sort()
+        
+        res = []
+        
+        last = intervals[0]
+        
+        for interval in intervals:
+            if interval[0] <= last[1]:
+                last[1] = max(interval[1], last[1])
+            else:
+                res.append(last)
+                last = interval
+        res.append(last)
+
+if __name__ == '__main__':
+    intervals = [[1,3],[6,9]]
+    newInterval = [2,5]
+    # output = [[1,5],[6,9]]
+    print(Solution.insert(None,intervals, newInterval)
